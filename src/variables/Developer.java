efbcf398ba15f73484f8b45b8f75b5ac0ae77b51
jramlope <jramlope@everis.com>
2021-05-26 08:27:37 +0200

    Clase Developer actualizada

--- src/variables/Developer.java
+++ src/variables/Developer.java
@@ -1,83 +1,95 @@
 package variables;
 
 /**
- * Clase Developer
+ * Clase que determina un desarrollador
  */
 public class Developer {
 
-	/** Atributos privados*/
+	/** Número de empleados */
 	private final static int numOfEmployees = 4;
+	
+	/** ID de empleado*/
 	private int employeId;
+	
+	/** Número de días de vacaciones*/
 	private static int daysOfVacations = 21;
+	
+	/** Nombre de la empresa, que será Everis por defecto*/
 	private final static String nameOfCompany = "Everis";
+	
+	/** Nombre de los empleados*/
 	private String nameOfEmployee;
+	
+	/** Número total de empleados*/
 	private static int allEmployees = 127;
+	
+	/** Número de días libres*/
 	private int freeDays;
 
-	/** Generación del constructor*/
+	/** Generación del constructor */
 	public Developer(int employeId, String nameOfEmployee) {
 		this.employeId = employeId;
 		this.nameOfEmployee = nameOfEmployee;
 	}
 
 	// Generación de los métodos getters y setters
-	
-	/** Método encargado de devolvernos el valor del atributo EmployeId*/
+
+	/** Método encargado de devolvernos el valor del atributo EmployeId */
 	public int getEmployeId() {
 		return employeId;
 	}
 
-	/** Método encargado de modificar el valor del atributo EmployeId*/
+	/** Método encargado de modificar el valor del atributo EmployeId */
 	public void setEmployeId(int employeId) {
 		this.employeId = employeId;
 	}
 
-	/** Método encargado de devolvernos el valor del atributo DaysOfVacations*/
+	/** Método encargado de devolvernos el valor del atributo DaysOfVacations */
 	public static int getDaysOfVacations() {
 		return daysOfVacations;
 	}
 
-	/** Método encargado de modificar el valor del atributo DaysOfVacations*/
+	/** Método encargado de modificar el valor del atributo DaysOfVacations */
 	public static void setDaysOfVacations(int daysOfVacations) {
 		Developer.daysOfVacations = daysOfVacations;
 	}
 
-	/** Método encargado de devolver el valor del atributo NameOfEmployee*/
+	/** Método encargado de devolver el valor del atributo NameOfEmployee */
 	public String getNameOfEmployee() {
 		return nameOfEmployee;
 	}
 
-	/** Método encargado de modificar el valor del atributo NameOfEmployee*/
+	/** Método encargado de modificar el valor del atributo NameOfEmployee */
 	public void setNameOfEmployee(String nameOfEmployee) {
 		this.nameOfEmployee = nameOfEmployee;
 	}
 
-	/** Método encargado de devolver el valor del atributo AllEmployees*/
+	/** Método encargado de devolver el valor del atributo AllEmployees */
 	public static int getAllEmployees() {
 		return allEmployees;
 	}
 
-	/** Método encargado de modificar el valor del atributo AllEmployees*/
+	/** Método encargado de modificar el valor del atributo AllEmployees */
 	public static void setAllEmployees(int allEmployees) {
 		Developer.allEmployees = allEmployees;
 	}
 
-	/** Método encargado de devolver el valor del atributo NumOfEmployees*/
+	/** Método encargado de devolver el valor del atributo NumOfEmployees */
 	public static int getNumofemployees() {
 		return numOfEmployees;
 	}
 
-	/** Método encargado de modificar el valor del atributo NumOfEmployees*/
+	/** Método encargado de modificar el valor del atributo NumOfEmployees */
 	public static String getNameofcompany() {
 		return nameOfCompany;
 	}
 
-	/** Método encargado de devolver el valor del atributo FreeDays*/
+	/** Método encargado de devolver el valor del atributo FreeDays */
 	public int getFreeDays() {
 		return freeDays;
 	}
 
-	/** Método encargado de modificar el valor del atributo FreeDays*/
+	/** Método encargado de modificar el valor del atributo FreeDays */
 	public void setFreeDays(int freeDays) {
 		this.freeDays = freeDays;
 	}
